eab005d0fe4383b81f985907b8d3bec74757dc37
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-07-28 17:20:54 -0300

    criação do pacote controller que eu tinha esquecido de criar

---
+++ route/src/main/java/com/bus/route/controllers/BusController.java
@@ -0,0 +1,9 @@
+package com.bus.route.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/driver")
+public class BusController {
+}
